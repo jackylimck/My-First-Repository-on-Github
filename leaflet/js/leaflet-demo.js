1b5753121d5fa3c54558cdeca80e5bec879e5bf2
jackylimck <jackylimck@hotmail.com>
2015-10-28 14:10:59 +0800

    Leaflet Example
    Signed-off-by: jackylimck <jackylimck@hotmail.com>

---
+++ leaflet/js/leaflet-demo.js
@@ -0,0 +1,14 @@
+var map = L.map( 'map', {
+    center: [20.0, 110.0],
+    zoom: 3
+});
+
+L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="" height="16">',
+    subdomains: ['otile1','otile2','otile3','otile4']
+}).addTo( map );
+
+for ( var i=0; i < markers.length; ++i ) 
+{
+   L.marker([markers[i].lat, markers[i].lng]).bindPopup(markers[i].name).addTo(map);
+}
