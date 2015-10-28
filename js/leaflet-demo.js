b732ccd57d140b7934177a85fef0a06434cd6abf
jackylimck <jackylimck@hotmail.com>
2015-10-28 17:27:56 +0800

    final commit for 28/10/15

--- js/leaflet-demo.js
+++ js/leaflet-demo.js
@@ -5,7 +5,7 @@ var map = L.map( 'map', {
 
 L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="" height="16">',
-    subdomains: ['otile1','otile2','otile3','otile4']
+    subdomains: ['otile1','otile2','otile3','otile4','otile5']
 }).addTo( map );
 
 for ( var i=0; i < markers.length; ++i ) 
